d0d130be748e121f34b36044e471203a59ac0087
Islom786 <abdusalomovislom057@gmail.com>
2023-11-18 16:04:35 +0500

    Questions

---
+++ masalalar/js/app.js
@@ -0,0 +1,95 @@
+// 1-masala:
+// let sum = +prompt('Summani kiriting!')
+// let arr = []
+// const sumInput = function(num) {
+//     arr.push(num);
+//     if (arr.includes(NaN)) {
+//         return arr.shift();
+//     }
+// }
+// sumInput(sum);
+// console.log(arr);
+
+// console.log(String() + Number());
+// let pro = +prompt('ha');
+// if (isNaN(pro)) {
+//     console.log('Nega');
+// } else {
+//     console.log(pro.trim());
+// }
+
+// 2-masala:
+// let arr = ['Jazz', 'Blues']
+// console.log(arr);
+// arr.push("Rock-n-roll");
+// console.log(arr);
+// for(i = 1; i < arr.length; i += 2) {
+//     arr[i] = "Classic";
+// }
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift("Rap", "Reggae");
+// console.log(arr);
+
+// 3-masala:
+// let salaries = {
+// 	"Akmal": 1200,
+// 	"Salim": 5200,
+// 	"Karima": 1800
+// }
+// const sumSalaries = function(sum) {
+//     let sumArr = Object.values(sum);
+//     let count = 0;
+//     for(let item of sumArr) {
+//         count = count + item;
+//     }
+//     return count;
+// }
+// console.log(sumSalaries(salaries));
+
+// 4-masala:
+// let numbers = [1, 2, 3, 4, 5]
+// const calcSum = (number) => {
+//     if (number.includes(null)) {
+//         number.pop();
+//     } else {
+//         number;
+//     }
+//     let firstCount = 0;
+//     let secondCount = 0;
+//     for(let num of number) {
+//         if (num % 2 === 0) {
+//             firstCount = firstCount + num;
+//         } else {
+//             secondCount = secondCount + num;
+//         }
+//     }
+//     let sum = []
+//     sum.push(firstCount);
+//     sum.push(secondCount);
+//     if (sum.includes(0)) {
+//         sum.pop()
+//         sum.shift()
+//     } else {
+//         sum;
+//     }
+//     return sum;
+// }
+// console.log(calcSum(numbers));
+
+// 5-masala:
+// let numbers = [-3, -2, 1, 5]
+// let nums = []
+// let sumNums = []
+// for(i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+//     nums.push(i)
+// }
+// for(let sum of nums) {
+//     if (!numbers.includes(sum)) {
+//         sumNums.push(sum);
+//     }
+// }
+// console.log(sumNums);
+// console.log(nums);
+// console.log(numbers);
